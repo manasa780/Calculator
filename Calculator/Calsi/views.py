7d75252a6a482605eefe92021b36a1474d4e0513
manasa780 <koremanasa570@gmail.com>
2025-05-04 11:40:35 +0530

    Project Upload to cloud

---
+++ Calculator/Calsi/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def calculate(request):
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.POST.get('num1', 0))
+        num2 = float(request.POST.get('num2', 0))
+        operation = request.POST.get('operation')
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Cannot divide by zero"
+
+    return render(request, 'calculator/calculator.html', {'result': result})
